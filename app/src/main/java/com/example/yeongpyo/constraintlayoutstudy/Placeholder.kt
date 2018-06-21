e3363790e58aef146a1f79996d7555ec340d182b
yeongpyo <byp93@naver.com>
2018-06-21 23:33:49 +0900

    TabLayout Setting

--- app/src/main/java/com/example/yeongpyo/constraintlayoutstudy/Placeholder.kt
+++ app/src/main/java/com/example/yeongpyo/constraintlayoutstudy/Placeholder.kt
@@ -2,12 +2,24 @@ package com.example.yeongpyo.constraintlayoutstudy
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_placeholder.*
 
 class Placeholder : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return layoutInflater.inflate(R.layout.fragment_placeholder, container, false)
+        val view = layoutInflater.inflate(R.layout.fragment_placeholder, container, false)
+        intArrayOf(R.id.view1, R.id.view2, R.id.view3)
+                .forEach { view.findViewById<View>(it).setOnClickListener(::onclick) }
+        return view
     }
+
+    fun onclick(view: View){
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        viewholder.setContentId(view.id)
+    }
+
 }
